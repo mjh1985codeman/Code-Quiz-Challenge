e400071e8275436d3e3978708c4b68c649c7a41d
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-04 16:04:56 -0500

    added start and high score button DOM  elements

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var startButtonEl = document.querySelector("#start-quiz");
+console.log(startButtonEl);
+var highScoresButtonEl = document.querySelector("#highscores");
+console.log(highScoresButtonEl);
