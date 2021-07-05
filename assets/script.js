61c8bb06b661f90699e9e6ba1b8bf85408cb8ed6
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-05 00:44:20 -0500

    Added Question Variables

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,34 @@
-var startButtonEl = document.querySelector("#start-quiz");
-console.log(startButtonEl);
-var highScoresButtonEl = document.querySelector("#highscores");
-console.log(highScoresButtonEl);
+var quizQuestions = [
+    {
+        question: "The window.confirm function returns a value of _____",
+        possibleAnswers: ["true/false", "A Number", "The Browser Version", "user input"],
+        correctAnswer: "true/false"
+    },
+    {
+        question: "Placing conten between parentheses is called _____",
+        possibleAnswers: ["Parenting", "Object Manipulation", "Passing an argument into a function", "An Expression"],
+        correctAnswer: "Passing an argument into a function"
+    },
+    {
+        question: "In JavaScript in order for a Function to execute it must be defined and then _____",
+        possibleAnswers: ["called", "executed", "fired", "enacted"],
+        correctAnswer: "called"
+    },
+    {
+        question: "A ______ is a named location for a value that gets stored in the browser's memory when a program is run.",
+        possibleAnswers: ["place-holder", "variable", "directory", "cookie"],
+        correctAnswer: "variable"
+    },
+    {
+        question: "The infamous 'if...else' statement is an example of a ______ statement.",
+        possibleAnswers: ["direct", "propietary", "boolean", "conditional"],
+        correctAnswer: "conditional"
+    }
+];
+
+var beginQuiz = document.querySelector("#start-quiz");
+
+
+
+
+
