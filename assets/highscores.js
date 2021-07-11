388b3bd36b1d945ce6d4afcc2c123da7ca183a7e
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-07-10 23:34:43 -0500

    Pushed Local values to Score.HTML page

--- assets/highscores.js
+++ assets/highscores.js
@@ -1,22 +1,22 @@
 //Created functions to move user input from local storage back to the site.
 // vars to control DOM
-var storedScoresEl = document.getElementById("userTimeScore");
-var storedInitialsEl = document.getElementById("userInfo");
+var storedScoresEl = document.getElementById("stored-scores");
+var storedInitialsEl = document.getElementById("stored-initials");
 
-//function to order the getScores function
+//window.onload function to order the getScores function
 window.onload = function () {
   getScores();
 };
 // getting information from local storage.
 function getScores() {
-  //var loadUserInitials = JSON.parse(localStorage.getItem("userInfo"));
-  //var loadUserScores = JSON.parse(localStoreage.getItem("userTimeScore"));
+  var loadUserInitials = JSON.parse(localStorage.getItem("userInfo"));
+  var loadUserScores = JSON.parse(localStorage.getItem("userTimeScore"));
+  //How do I get these on the page?!
+  console.log(loadUserInitials);
+  console.log(loadUserScores);
 
-  // functions to load data from local storage to scores.html
-
-  // document.createAttribute("stored-scores").innerHTML = loadUserScores;
-  //document.createAttribute("stored-initials").innerHTML = loadUserInitials;
-  console.log("HOW DO I DO THIS?!");
+  storedScoresEl.textContent = "Score: " + loadUserScores;
+  storedInitialsEl.textContent = "Initials: " + loadUserInitials;
 }
 
 //window.onload();
